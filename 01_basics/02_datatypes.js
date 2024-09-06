db833a553aaf0944797f2e2d924fac4d4373b07a
Debojyoti10 <113207877+Debojyoti10@users.noreply.github.com>
2024-09-06 17:41:03 +0000

    container config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") we are using nodejs, not browser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("Debojyoti")
+
+
+let name = "Debojyoti"
+let age  = 21
+let isloggedIn = false
+
+// number => 2 to the power of 53
+// bigInt 
+// string =>""
+// boolean => trie/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+

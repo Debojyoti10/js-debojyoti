ded0af6b682f8b7999ccedf927674119a01352ab
Debojyoti10 <113207877+Debojyoti10@users.noreply.github.com>
2024-09-07 17:47:12 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //compariosn convert null to 0 while equality does not
+
+
+// ===
+console.log("2" === 2)

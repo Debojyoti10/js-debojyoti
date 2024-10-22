6a1eac7278bc575573153994b92ea2749f16588c
Debojyoti10 <113207877+Debojyoti10@users.noreply.github.com>
2024-10-22 20:20:34 +0000

    scopes & hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,7 +6,7 @@ if(true){
     const b = 20
     var c = 30
 
-    console.log("INNER: ",a);
+    //console.log("INNER: ",a);
 }
 /*
 for (let i = 0; i < Array.length; i++){
@@ -14,7 +14,46 @@ for (let i = 0; i < Array.length; i++){
 
 }
 */
-console.log(a);
+//console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+
+function one(){
+    const username = "debojyoti"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two();
+    
+}
+//one()
+
+
+if(true){
+    const username = "debojyoti"
+    if(username == "debojyoti"){
+        const website = "youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+//+++++++++++++++++++++++++++++++ interesting ++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
 
